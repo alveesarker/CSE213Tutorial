5a7e9c5148a1aa79fa0f190eb16bc4408d445149
alvee <asahanaz305@gmail.com>
2025-10-11 22:00:23 +0600

    new commit

--- Autumn25/Basic Java/Main.java
+++ Autumn25/Basic Java/Main.java
@@ -1,24 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        // System.out.println(2 * 3);
+    // System.out.println();
 
-        int a;
-        a = 10;
-        System.out.println(a);
 
-        String fName = "John ";
-        String lNname = "Doe";
-        String fullName = fName + lNname;
-        System.out.println(fullName);
-
-        System.out.println(2 + 3 + "4" + 6 + 3);
-
-        // short num = 1;
-        // System.out.println(num);
-
-        float num = 5.12349494593489f;
-        System.out.println(num);
 
     }
 }
