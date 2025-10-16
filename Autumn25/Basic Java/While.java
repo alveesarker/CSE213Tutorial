277844631435e2da9d4932c1823eee01300f1347
alvee <asahanaz305@gmail.com>
2025-10-16 18:20:49 +0600

    while loop

---
+++ Autumn25/Basic Java/While.java
@@ -0,0 +1,12 @@
+public class While {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 1000) {
+            System.out.println("Hello World");
+            // more lines
+        }
+        System.out.println("Hello java");
+
+    }
+}
