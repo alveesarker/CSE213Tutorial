24b10ab1842f1755106c1796a3f9efbe86900c00
alvee <asahanaz305@gmail.com>
2025-10-17 09:52:55 +0600

    conditional statement

--- Autumn25/Basic Java/Main.java
+++ Autumn25/Basic Java/Main.java
@@ -1,72 +1,72 @@
 public class Main {
     public static void main(String[] args) {
 
-        // int x = 5;
-        // System.out.println(x);
-        // x = 7;
-        // System.out.println(x);
+        /*int x = 5;
+        System.out.println(x);
+        x = 7;
+        System.out.println(x);*/
 
-        // type casting
-        // int myNum = 10;
-        // double dMynum = myNum;
-        // System.out.println(dMynum);
+        /*type casting
+        int myNum = 10;
+        double dMynum = myNum;
+        System.out.println(dMynum);*/
 
-        // double myNum = 10.2;
-        // int iMyNum = (int) myNum;
-        // System.out.println(iMyNum);
+        /*double myNum = 10.2;
+        int iMyNum = (int) myNum;
+        System.out.println(iMyNum);*/
 
-        // increment operator
-        // int x = 5;
-        // x = x + 1;
-        // ++x;
-        // ++x;
-        // x = x - 1;
-        // --x;
-        // System.out.println(x);
+        /*increment operator
+        int x = 5;
+        x = x + 1;
+        ++x;
+        ++x;
+        x = x - 1;
+        --x;
+        System.out.println(x);*/
 
-        // comparison operator
-        // int x = 7;
-        // int y = 5;
-        // System.out.println(x >= y);
+        /*comparison operator
+        int x = 7;
+        int y = 5;
+        System.out.println(x >= y);*/
 
-        // string
-        // String s = "ABCDEFGHIJ KLMNOPQRSTUVWXYZ";
-        // System.out.println(s.length());
-        // String h = "hello";
-        // String i = "hello";
-        // System.out.println(i.equals(h));
-        // if (x < y){
-        // System.out.println(y);
-        // } else{
-        // System.out.println(x);
-        // }
+        /*string
+        String s = "ABCDEFGHIJ KLMNOPQRSTUVWXYZ";
+        System.out.println(s.length());
+        String h = "hello";
+        String i = "hello";
+        System.out.println(i.equals(h));
+        if (x < y){
+        System.out.println(y);
+        } else{
+        System.out.println(x);
+        }*/
 
-        // int max = Math.max(x, y);
-        // System.out.println(max);
+        /*int max = Math.max(x, y);
+        System.out.println(max);
 
-        // System.out.println(Math.sqrt(64));
+        System.out.println(Math.sqrt(64));
 
-        // System.out.println(Math.round(4.5));
+        System.out.println(Math.round(4.5));
         int x = 10;
         int y = 6;
         int a = 30;
         int b = 40;
 
-        // if (x < y || a > b) {
-        //     // code
-        //     System.out.println("Hello");
-        // } else {
-        //     System.out.println("Hello java");
-        // }
+        if (x < y || a > b) {
+            // code
+            System.out.println("Hello");
+        } else {
+            System.out.println("Hello java");
+        }*/
         
-        // String result = x < y ? "Hello" : "Hello java";
-        // System.out.println(result);
-        // if (x > y) {
-        // System.out.println("Hello world");
-        // }
+        /*String result = x < y ? "Hello" : "Hello java";
+        System.out.println(result);
+        if (x > y) {
+        System.out.println("Hello world");
+        }*/
 
-        // if (x > y){
-        // System.out.println("Hello java");
-        // }
+        /*if (x > y){
+        System.out.println("Hello java");
+        }*/
     }
 }
