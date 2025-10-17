c5c723d05275029f8321f5f4e42dccb402c3ea03
alvee <asahanaz305@gmail.com>
2025-10-17 09:56:14 +0600

    for loop

--- Autumn25/Basic Java/For.java
+++ Autumn25/Basic Java/For.java
@@ -8,8 +8,16 @@ public class For {
         // // clodeblock to be executed
         // }
 
-        for (int i = 0 ; i < 5 ; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(i);
+        }
+
+        int[] numbers = { 1, 5, 10, 25 };
+        int sum = 0;
+
+        // Loop through the array and add each element to sum
+        for (int k = 0; k < numbers.length; k++) {
+            sum = sum + numbers[k];
         }
 
         // 1st s -> 2nd s(if true) -> codeblock -> 3rd s -> 2nd s(if true) -> codeblock -> 3rd s -> 2nd s(if false) -> out of the loop
