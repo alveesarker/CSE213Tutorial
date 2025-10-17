8e06294058b63ff52aec504a01cd8a49edb0f2a1
alvee <asahanaz305@gmail.com>
2025-10-17 09:54:45 +0600

    while loop

--- Autumn25/Basic Java/While.java
+++ Autumn25/Basic Java/While.java
@@ -1,54 +1,54 @@
 public class While {
     public static void main(String[] args) {
-        // int a = 1;
-        // int b = 10;
-        // while (a <= b) {
-        // System.out.println("Hello world");
-        // a = a + 1;
-        // // more lines
-        // }
-        // System.out.println("Hello java");
-        // loop
-        // int i = 0;
-        // while (i <= 10) {
-        // System.out.println(i);
-        // i = i + 1;
-        // }
+        /*int a = 1;
+        int b = 10;
+        while (a <= b) {
+        System.out.println("Hello world");
+        a = a + 1;
+        // more lines
+        }
+        System.out.println("Hello java");
+        loop
+        int i = 0;
+        while (i <= 10) {
+        System.out.println(i);
+        i = i + 1;
+        } */
 
         // ...1000
 
         // countdown
-        // int countdown = 3;
+        /*int countdown = 3;
 
-        // while (countdown > 0) {
-        // System.out.println(countdown);
-        // // countdown--;
-        // // line2
-        // // line3
-        // countdown = countdown - 1;
-        // }
+        while (countdown > 0) {
+        System.out.println(countdown);
+        // countdown--;
+        // line2
+        // line3
+        countdown = countdown - 1;
+        } */
 
         // System.out.println("Happy New Year!!");
 
-        // int i = 0;
-        // while (i <= 10) {
-        // System.out.println(i);
-        // i++;
-        // if (i == 7){
-        // break;
-        // }
-        // // line
-        // }
+        /*int i = 0;
+        while (i <= 10) {
+        System.out.println(i);
+        i++;
+        if (i == 7){
+        break;
+        }
+        // line
+        } */
 
-        // int i = 0;
-        // while (i <= 5) {
-        //     i++;
-        //     if (i == 3) {
-        //         continue;
-        //     }
-        //     System.out.println(i);
-        //     // line
-        // }
+        /*int i = 0;
+        while (i <= 5) {
+            i++;
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i);
+            // line
+        } */
 
     }
 }
